1b96fd762659556432585f490138f9be68172491
nelsonchung <nelson_chung@askey.com>
2020-02-11 11:23:56 +0800

    Fix error output

--- sizeof/main.c
+++ sizeof/main.c
@@ -4,17 +4,17 @@ int main(int argc, char* argv[])
 {
     char buf = 0;
     int buf_length = sizeof(buf);
-    printf("The length is buf is %d\n", buf_length);
+    printf("The length of buf is %d\n", buf_length);
 
     char buf_1[20] = {0};
     buf_length = sizeof(buf_1);
-    printf("The length is buf_1 is %d\n", buf_length);
+    printf("The length of buf_1 is %d\n", buf_length);
 
     char buf_2[10] = {0};
     buf_length = sizeof(buf_1);
-    printf("The length is buf_2 is %d\n", buf_length);
+    printf("The length of buf_2 is %d\n", buf_length);
 
     buf_length = sizeof(char);
-    printf("The length is char is %d\n", buf_length);
+    printf("The length of char is %d\n", buf_length);
     return 0;
 }
