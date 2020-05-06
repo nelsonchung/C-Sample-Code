5d69c2c7bb13de60382b5216af20ec6c3909521c
Nelson Chung <nelson2_chung@compalbn.com>
2020-05-06 21:33:38 +0800

    support getopt

--- getopt_long/getopt_long.c
+++ getopt_long/getopt_long.c
@@ -4,18 +4,20 @@
 char *l_opt_arg;  
 //char* const short_options = "nbl:";  
 struct option long_options[] = {  
-    { "name",     0,   NULL,    'n'     },  
-    { "phonenumber",  0,   NULL,    'p'     },  
-    { "test",     1,   NULL,    't'     },  
-    {      0,     0,     0,     0},  
+    { "name",           0,   NULL,    'n'     },  
+    { "phonenumber",    0,   NULL,    'p'     },  
+    { "test",           1,   NULL,    't'     },  
+    { "help",           0,   NULL,    'h'     },  
+    {      0,           0,     0,     0},  
 };  
 
 int main(int argc, char *argv[])  
 {  
     int c;  
     //while((c = getopt_long (argc, argv, short_options, long_options, NULL)) != -1)  
+    const char *optstring = "npt:h";
     int option_index=0;
-    while((c = getopt_long (argc, argv, "", long_options, &option_index)) != -1)  
+    while((c = getopt_long (argc, argv, optstring, long_options, &option_index)) != -1)  
     {  
         switch (c)  
         {  
@@ -29,6 +31,14 @@ int main(int argc, char *argv[])
             l_opt_arg = optarg;  
             printf("This is test from user input %s!\n", l_opt_arg);  
             break;  
+            case 'h':
+            printf("Support -n\n");
+            printf("        -p\n");
+            printf("        -t: Need to input parameter\n");
+            break;
+            default:
+            printf(" -h for more help information");
+            break;
         }  
     }  
     return 0;  
